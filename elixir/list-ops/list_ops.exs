0cca8d127cff0b014336e3f4581a58e52c8989b0
Christophe Bliard <christophe.bliard@hiptest.net>
2016-07-13 10:35:08 +0200

    elixir/list-ops: implemented

--- elixir/list-ops/list_ops.exs
+++ elixir/list-ops/list_ops.exs
@@ -7,38 +7,44 @@ defmodule ListOps do
   # automatically imported) and so shouldn't be used either.
 
   @spec count(list) :: non_neg_integer
-  def count(l) do
-
-  end
+  def count([]), do: 0
+  def count([_ | tail]), do: 1 + count(tail)
 
   @spec reverse(list) :: list
-  def reverse(l) do
-
+  def reverse(list) do
+    reverse_list(list, [])
   end
+
+  defp reverse_list([], acc), do: acc
+  defp reverse_list([head | tail], acc), do: reverse_list(tail, [head | acc])
 
   @spec map(list, (any -> any)) :: list
-  def map(l, f) do
-
-  end
+  def map([], _), do: []
+  def map([head | tail], f), do: [f.(head) | map(tail, f)]
 
   @spec filter(list, (any -> as_boolean(term))) :: list
-  def filter(l, f) do
-
+  def filter([], _), do: []
+  def filter([head | tail], f) do
+    if f.(head) do
+      [head | filter(tail, f)]
+    else
+      filter(tail, f)
+    end
   end
 
   @type acc :: any
   @spec reduce(list, acc, ((any, acc) -> acc)) :: acc
-  def reduce(l, acc, f) do
-
+  def reduce([], acc, _), do: acc
+  def reduce([head | tail], acc, f) do
+    reduce(tail, f.(head, acc), f)
   end
 
   @spec append(list, list) :: list
-  def append(a, b) do
-
-  end
+  def append([], []), do: []
+  def append([], l), do: l
+  def append([head | tail], l), do: [head | append(tail, l)]
 
   @spec concat([[any]]) :: [any]
-  def concat(ll) do
-
-  end
+  def concat([]), do: []
+  def concat([head | tail]), do: append(head, concat(tail))
 end
